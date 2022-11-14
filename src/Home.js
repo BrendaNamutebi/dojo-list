6201acd78d1fbaea2b0cffc8ed8cda91a022a738
BrendaNamutebi <brendanamutebi23@gmail.com>
2022-11-14 14:10:27 +0300

    fetched data from server

--- src/Home.js
+++ src/Home.js
@@ -3,28 +3,24 @@ import { useState ,useEffect} from 'react'
 import BlogList from './BlogList';
 
 const Home = () => {
-  const[blogs,setBlogs] =useState([
-    {title: 'My new website',body:'lorem ipsum...',author:'mario',id:1},
-    {title: 'Welcome party!',body:'lorem ipsum...',author:'brenda',id:2},
-    {title: 'Web dev top tips',body:'lorem ipsum...',author:'mario',id:3},
-  ]);
+  const[blogs,setBlogs] =useState(null);
      
   const [name,setName] =useState('mario')
 
-    const handleDelete =(id)=>{
-     const newBlogs = blogs.filter(blog => blog.id !== id)
-     setBlogs (newBlogs);
-    }    
+     
     useEffect(() =>{
-      console.log('úse effect ran')
-      console.log(name);
-    },[name]);
+      fetch('http://localhost:8000/blogs')
+      .then(resp=>{
+        return resp.json();
+      })
+       .then(data=>{
+         setBlogs(data)
+       });
+    },[]);
    
   return ( 
     <div className='home'>
-         <BlogList blogs={blogs}  title ='All blogs' handleDelete={handleDelete}/>
-         <button onClick={() => setName('brenda')}>change name</button>
-         <p>{name}</p>
+       {blogs && <BlogList blogs={blogs} title="All blogs!" />}
       </div>
   );
 }
